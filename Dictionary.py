b5ba7bd95b7681354db692a543ca4d45c1d2fc39
JohnCobra33 <157510301+JohnCobra33@users.noreply.github.com>
2024-01-26 09:53:18 +0000

    Dictionary.py

--- Dictionary.py
+++ Dictionary.py
@@ -1,10 +1,15 @@
-#"Create a script that prompts for a number and creates a dictionary where the keys range from 1 to the specified number, and the values are the squares of the keys."
+# Solicita al usuario que ingrese un número y lo convierte a entero
+numero = int(input("Dime un número:"))
 
-number = int(input("Enter a number: "))
-squares = {}
+# Crea un diccionario llamado 'cuadrados'
+cuadrados = {}
 
-for num in range(1, number + 1):
-    squares[num] = num ** 2
+# Itera a través de los números desde 1 hasta el número ingresado por el usuario (inclusive)
+for num in range(1, numero + 1):
+    # Calcula el cuadrado de cada número y lo almacena en el diccionario 'cuadrados'
+    cuadrados[num] = num ** 2
 
-for num, value in squares.items():
-    print("%d -> %d" % (num, value))
+# Itera a través de los elementos del diccionario 'cuadrados'
+for num, valor in cuadrados.items():
+    # Imprime el número y su cuadrado en el formato especificado
+    print("%d -> %d" % (num, valor))
